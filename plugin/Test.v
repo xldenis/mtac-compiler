0cde8a627c7cde696cf2d4dd74c5dc9870b83f69
Xavier Denis <xldenis@gmail.com>
2019-02-11 16:44:33 +0100

    problem with fix'

--- plugin/Test.v
+++ plugin/Test.v
@@ -18,19 +18,27 @@ Definition isAnd (a : Prop) : Mtac bool :=
     end
   .
 
-  Definition tauto_simpl (prop : Prop) : Mtac prop :=
-    isT <- unify True prop;
-    match isT with
-    | left prf => @eq_rect Prop True (Mtac) (ret I) prop prf
-    | right _ => fail "omg"
-    end
+
+Notation "'mfix' f ( x : A ) : 'M' T := b" := (fix' (fun x=>T) (fun f (x : A)=>b))
+  (at level 85, f at level 0, x at next level, format
+  "'[v  ' 'mfix'  f  '(' x  ':'  A ')'  ':'  'M'  T  ':=' '/  ' b ']'").
+
+Definition simpl_prop_auto :=
+  mfix f (p : Prop) : M (p : Prop) := (* problem is here*)
+    fail "omg"
+  .
+
+Definition tauto_simpl (prop : Prop) : Mtac prop :=
+    mfix f (x : Prop) : M (x : Prop) :=
+      fail "omg"
+
      .
 Goal True.
 Proof.
  run (MtacLite.print "omg") as omg.
  run (MtacLite.unify False True) as omg2.
  run (tauto_simpl True) as om22.
- 
+
  run (MtacLite.ret 1) as om3.
  exact om22.
  (*run (isAnd (1 = 2 /\ 3 = 2)) as om4.*)
