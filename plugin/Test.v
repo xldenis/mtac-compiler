7b2c137e39461d03242d280b294f40aab158b1e8
Xavier Denis <xldenis@gmail.com>
2019-02-20 14:15:38 +0100

    Update evar map when posing mtaclite

--- plugin/Test.v
+++ plugin/Test.v
@@ -5,7 +5,9 @@ Require Import MtacLite.MtacLite.
 
 Import MtacLite.MtacLiteNotations.
 
-(*Check print "pomg".*)
+Import Coq.Strings.String.
+
+Check print "pomg".
 
 Definition isAnd (a : Prop) : Mtac bool :=
     x <- evar;
@@ -13,8 +15,8 @@ Definition isAnd (a : Prop) : Mtac bool :=
 
     eq <- unify a (x /\ y);
     match eq with
-    | left prf => ret true
-    | right prf => ret false
+    | Some prf => ret true
+    | Nothing => ret false
     end
   .
 
@@ -22,36 +24,6 @@ Notation "'mfix' f ( x : A ) : 'M' T := b" := (fix' (fun x=>T) (fun f (x : A)=>b
   (at level 85, f at level 0, x at next level, format
   "'[v  ' 'mfix'  f  '(' x  ':'  A ')'  ':'  'M'  T  ':=' '/  ' b ']'").
 
-Lemma True_neq_False : (True <> False).
-Proof.
-  unfold not.
-  intros.
-  rewrite <- H.
-  exact I.
-Qed.
-
-Set Printing All.
-
-Print True_neq_False.
-
-Check eq_ind.
-
-Lemma True_eq_True : (True = True).
-  run (unify True True) as omg.
-  destruct omg.
-
-  - exact e.
-  - contradiction.
-Qed.
-
-
-(*   run (unify True False) as omg. *)
-(*   destruct omg. *)
-(*   - exfalso. rewrite <- e. exact I. *)
-(*   - exact n. *)
-(* Qed. *)
-
-Print True_neq_False.
 Goal True.
 Proof.
   compile (print "gmo") as t1.
@@ -69,10 +41,7 @@ Proof.
   pose (@eq_refl _ (~ False)).
   unfold not in e.
   simpl in e.
-  native_compute in om222.
   compile (print "omg2") as tt.
   run (MtacLite.ret 1) as om3.
-  exact om22.
   run (isAnd (1 = 2 /\ 3 = 2)) as om4.
-
-
+Qed.
